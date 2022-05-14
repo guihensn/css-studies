2bdebc29ded97458ed6d6cb44e68461696c792b0
Guilherme Henrique <guihensn@gmail.com>
2022-05-14 15:16:03 -0300

    Sounds of typing and code improving

--- quotes-machine/quoteGenerator.js
+++ quotes-machine/quoteGenerator.js
@@ -1,32 +1,33 @@
 let quoteElement = document.getElementById('quote');
 let authorElement = document.getElementById('author');
 let newQuoteElement = document.getElementById('newQuote');
-newQuoteElement.addEventListener('mousedown',genarateQuote);
 
-genarateQuote();
+let audio = new Audio('./sounds/quote.wav');
 
+audio.volume = 0.3;
+audio.loop = true;
 
-function genarateQuote(){
+newQuoteElement.addEventListener('mousedown',start);
+
+function start(){
     hideElement(newQuoteElement);
 
     fetch("https://api.quotable.io/random")
-    .then((result) => result.json())
-    .then((data) => {
-        showQuote(data.content,data.author)
-    })
-
+        .then((result) => result.json())
+        .then((data) => {
+            audio.play();
+            resetText();
+            startShowingQuotes(data.content,data.author)
+        })
 }
 
 function hideElement(element){
     element.style = 'display: none'
 }
 
-
-function showQuote(quote, author){
-    resetText()
-    typeTexte(quote, quoteElement,0);
+function startShowingQuotes(quote, author){
     authorElement.innerText = author;
-
+    startTyping(quote, quoteElement,0);
 }
 
 function resetText(){
@@ -35,15 +36,19 @@ function resetText(){
 }
 
 
-function typeTexte(textArray, element, i){
-    
-        if(i < textArray.length){
-            element.innerHTML += textArray.charAt(i);
-            i++;
-            setTimeout(()=>{typeTexte(textArray, element,i)},100);
-        } else{
-            newQuoteElement.style = '';
-        }
-    
+function startTyping(textArray, element, i){
+    if(i > textArray.length){
+        resolve();
+        return;
+    } 
+
+    element.innerHTML += textArray.charAt(i);
+
+    i++;
+    setTimeout(()=>{startTyping(textArray, element,i)},100);
 }
 
+function resolve(){
+    newQuoteElement.style = '';
+    audio.pause();
+}
